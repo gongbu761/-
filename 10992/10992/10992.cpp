9c7c01e628cdaa2d6a3e44d226979236a9bd9072
gongbu761 <gongbu761@gmail.com>
2019-07-10 21:43:46 +0900

    성공

---
+++ 10992/10992/10992.cpp
@@ -0,0 +1,40 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 
+#include<stdio.h>
+int main() {
+	int a = 0;
+	scanf("%d", &a);
+	int t = 0;
+	int n = 0; // 출력갯수
+
+	for (t = 0;t < a; t++) { //입력한 갯수만큼 줄 출력
+		//마지막은 전부 출력
+		n = 2 * a - 1;
+		if (t == a - 1) {
+			while (n > 0) {
+				printf("*");
+				n--;
+			}
+			break;
+		}
+		 
+
+		while (n > 0) {
+
+			if (n == a - t) {
+				printf("*");break;
+			}
+			else if (n == a + t) {
+
+				printf("*"); 
+			}
+			else {
+				printf(" ");
+			}
+			n--;
+		}
+
+		printf("\n");
+	}
+
+	return 0;
+}
